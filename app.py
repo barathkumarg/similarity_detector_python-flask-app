07379776f92788c62192fefbf04975d57c336574
BARATHKUMAR G <62948487+barathkumarking@users.noreply.github.com>
2021-07-05 21:46:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for, session, Response, flash, jsonify
 from difflib import SequenceMatcher
 from werkzeug.utils import secure_filename
+import random
 app = Flask(__name__)
 import os
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     return render_template('index.html')
+@app.route('/getdata', methods=['POST'])
+def getdata():
+    try:
+        content = request.get_json()
+        # data processing code
+        # content['p'] -> textarea 1 content
+        # content['q'] -> textarea 2 content
+        similarity = SequenceMatcher(None, content['p'], content['q']).ratio()
+        similarity = similarity * 100
+        similarity = int(similarity)
+        sim=str(similarity)
+        return jsonify({
+            'status': True,
+            'percentage': similarity,
+            'result': "Your data is measured to be nearly "+ sim +"% matchably"
+        })
 
-@app.route("/check_similarity", methods=["POST", "GET"])
-def home():
-    sim=''
-    sim2=''
-    if request.method == 'POST' and 'text1' in request.form and 'text2' in request.form:
-        text1=request.form['text1']
-        text2=request.form['text2']
-        similarity=SequenceMatcher(None,text1,text2).ratio()
-        similarity=similarity*100
-        similarity=round(similarity, 2)
+    except Exception as e:
+        print(e)
+        return jsonify({
+            'status': False,
+            'result': "Error in processing"
+            })
 
-        return render_template('index.html',sim="It has {0} % Similarity Between them".format(similarity))
 
-    elif request.method == 'POST':
-        f1 = request.files['myfile1']
-        f2= request.files['myfile2']
-        filename1 = secure_filename(f1.filename)
-        f1.save(secure_filename(f1.filename))
 
-        filename2 = secure_filename(f2.filename)
-        f2.save(secure_filename(f2.filename))
-
-        with open(filename1,'rb') as file1, open(filename2,'rb') as file2:
-            file1data=file1.read()
-            file2data=file2.read()
-            similarity = SequenceMatcher(None, file1data, file2data).ratio()
-            similarity = similarity * 100
-            similarity = round(similarity, 2)
-
-        return render_template('index.html', sim2="It has {0} % Similarity Between them".format(similarity))
-    else:
-
-        return render_template('index.html',sim=sim,sim2=sim2)
 
 
 
